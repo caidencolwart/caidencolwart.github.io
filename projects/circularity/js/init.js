c45b70cb01daeaa8e0ed81ecc3ba8ad0b9d3e0d6
caidencolwart <caidencolwart@gmail.com>
2023-03-01 19:43:03 +0000

    new

--- projects/circularity/js/init.js
+++ projects/circularity/js/init.js
@@ -21,29 +21,33 @@ var init = function (window) {
         
         // TODO 1 : Declare and initialize our variables
 
-            var circle = 1; //variable to hold a single circle
-            var circles=[]; // Variable to store all the circles into one array
+            var circle = 1; //declaring a variable to hold one circle
+            var circles=[]; // creating an array to hold all of the circles
 
         // TODO 2 : Create a function that draws a circle 
 
+            // declaring a function called drawCircle that has the code to create a circle
            function drawCircle () {
             // Code to draw a circle
-            circle = draw.randomCircleInArea(canvas, true, true, '#999', 2); //draws a circle with the given parameters
-            physikz.addRandomVelocity(circle, canvas, 10, 10); //gives moition to canvas drawings, also gives random velocity and direction to the circles
-            view.addChild(circle); //allows the circle appear on the screen
-            circles.push(circle); //saves each new circle using the push method
+            circle = draw.randomCircleInArea(canvas, true, true, '#999', 2); //draws a random circle with a random  color, size, and location on the screen
+            physikz.addRandomVelocity(circle, canvas, 5, 10); //use the phyziks library to provide motion ,a random velocity, and diretion to the circles
+            view.addChild(circle); //allows the circle appear on the screen, makes the circle a child of view
+            circles.push(circle); //saves each new circle into an array using the push method
            }
 
         // TODO 3 / 7 : Call the drawCircle() function 
-        /*
-           drawCircle():
-           drawCircle():
-           drawCircle():
-           drawCircle():
-           drawCircle():
-        */
-           for (var circlesCreated = 0; circlesCreated < 100; circlesCreated++) {
-            drawCircle();
+
+            /*       call the drawCircle function and creates five circles
+            drawCircle():
+            drawCircle():
+            drawCircle():
+            drawCircle():
+            drawCircle():
+            */
+
+            //creates a for loop to call the drawCircle function 100 times
+           for (var circlesCreated = 0; circlesCreated <= 100; circlesCreated++) {
+                drawCircle();
            }
 
         ////////////////////////////////////////////////////////////
@@ -58,7 +62,7 @@ var init = function (window) {
         function update() {
             // TODO 4 : Update the circle's position //
 
-            /*
+            /*     call the physiks.updatePostition to the circles array, calls the first 5 circles
             physikz.updatePosition(circles[0]);
             physikz.updatePosition(circles[1]);
             physikz.updatePosition(circles[2]);
@@ -68,7 +72,7 @@ var init = function (window) {
 
             // TODO 5 / 10 : Call game.checkCirclePosition() on your circles.
 
-            /*
+            /*     code that checks the circles position to make sure it stays on the screen
            game.checkCirclePosition(circles[0]);
            game.checkCirclePosition(circles[1]);
            game.checkCirclePosition(circles[2]);
@@ -76,14 +80,15 @@ var init = function (window) {
            game.checkCirclePosition(circles[4]);
            */
 
+           // creates a for loop to move 100 circles
            for (var i = 0; i < circles.length; i++) {
-            var eachCircle = circles[i];
-            physikz.updatePosition(eachCircle);
+            var eachCircle = circles[i]; 
+            physikz.updatePosition(eachCircle); //Applies the physiks.updatePosition function to each circle
            }
 
             // TODO 9 : Iterate over the array
 
-          game.checkCirclePosition(eachCircle);
+          game.checkCirclePosition(eachCircle); // call the game.checkCirclePosition to check eachCircle made in the for loop
             
         }
     
